9e0f9536fc779ea27b6bb24e6ffdd774c58b40e1
Roger Mateus <rmateus495@gmail.com>
2026-05-30 10:31:49 -0300

    Atualiza laboratorio 03 com menu interativo

--- WACAD008-TypeScript/lab03/src/index.ts
+++ WACAD008-TypeScript/lab03/src/index.ts
@@ -1,5 +1,4 @@
-
-
+import * as readline from "readline";
 import { Carrinho } from "./Carrinho";
 import { TV } from "./TV";
 import { Celular } from "./Celular";
@@ -7,33 +6,134 @@ import { Bicicleta } from "./Bicicleta";
 
 const carrinho = new Carrinho();
 
-const tv = new TV(
-    "Samsung Crystal",
-    "4K",
-    55,
-    "Samsung",
-    2500
-);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-const celular = new Celular(
-    "Galaxy S24",
-    "256GB",
-    "Samsung",
-    4500
-);
+function menu(): void {
+    console.log("\n===== MENU =====");
+    console.log("1 - Adicionar TV");
+    console.log("2 - Adicionar Celular");
+    console.log("3 - Adicionar Bicicleta");
+    console.log("4 - Ver Carrinho");
+    console.log("0 - Sair");
 
-const bicicleta = new Bicicleta(
-    "Caloi Explorer",
-    29,
-    "Caloi",
-    1800
-);
+    rl.question("Escolha uma opção: ", (opcao: string) => {
 
-console.log("Adicionando TV...");
-carrinho.adicionarProduto(tv);
+        switch (opcao) {
 
-console.log("Adicionando Celular...");
-carrinho.adicionarProduto(celular);
+            case "1":
+                adicionarTV();
+                break;
 
-console.log("Adicionando Bicicleta...");
-carrinho.adicionarProduto(bicicleta);
+            case "2":
+                adicionarCelular();
+                break;
+
+            case "3":
+                adicionarBicicleta();
+                break;
+
+            case "4":
+                carrinho.exibirCarrinho();
+                menu();
+                break;
+
+            case "0":
+                rl.close();
+                break;
+
+            default:
+                console.log("Opção inválida.");
+                menu();
+        }
+    });
+}
+
+function adicionarTV(): void {
+
+    rl.question("Modelo: ", (modelo: string) => {
+
+        rl.question("Resolução: ", (resolucao: string) => {
+
+            rl.question("Tamanho: ", (tamanho: string) => {
+
+                rl.question("Fabricante: ", (fabricante: string) => {
+
+                    rl.question("Valor: ", (valor: string) => {
+
+                        const tv = new TV(
+                            modelo,
+                            resolucao,
+                            Number(tamanho),
+                            fabricante,
+                            Number(valor)
+                        );
+
+                        carrinho.adicionarProduto(tv);
+
+                        console.log("TV adicionada.");
+                        menu();
+                    });
+                });
+            });
+        });
+    });
+}
+
+function adicionarCelular(): void {
+
+    rl.question("Modelo: ", (modelo: string) => {
+
+        rl.question("Memória: ", (memoria: string) => {
+
+            rl.question("Fabricante: ", (fabricante: string) => {
+
+                rl.question("Valor: ", (valor: string) => {
+
+                    const celular = new Celular(
+                        modelo,
+                        memoria,
+                        fabricante,
+                        Number(valor)
+                    );
+
+                    carrinho.adicionarProduto(celular);
+
+                    console.log("Celular adicionado.");
+                    menu();
+                });
+            });
+        });
+    });
+}
+
+function adicionarBicicleta(): void {
+
+    rl.question("Modelo: ", (modelo: string) => {
+
+        rl.question("Aro: ", (aro: string) => {
+
+            rl.question("Fabricante: ", (fabricante: string) => {
+
+                rl.question("Valor: ", (valor: string) => {
+
+                    const bicicleta = new Bicicleta(
+                        modelo,
+                        Number(aro),
+                        fabricante,
+                        Number(valor)
+                    );
+
+                    carrinho.adicionarProduto(bicicleta);
+
+                    console.log("Bicicleta adicionada.");
+                    menu();
+                });
+            });
+        });
+    });
+}
+
+menu();
