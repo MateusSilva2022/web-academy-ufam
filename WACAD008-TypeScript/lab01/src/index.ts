e8fa9d6fbf197ae3399ff67758935ea232906270
Roger Mateus <rmateus495@gmail.com>
2026-05-29 22:52:57 -0300

    Atualiza laboratorio 01 com menu interativo

--- WACAD008-TypeScript/lab01/src/index.ts
+++ WACAD008-TypeScript/lab01/src/index.ts
@@ -1,36 +1,107 @@
-import { Lembrete } from "./lembrete";
+import * as readline from "readline";
 import {
     adicionarLembrete,
     listarLembretes,
     editarLembrete,
     removerLembrete
 } from "./sistema";
+import { Lembrete } from "./lembrete";
 
-const lembrete1: Lembrete = [
-    "Estudar TypeScript",
-    "21/05/2026 15:00",
-    "25/05/2026",
-    "Fazer exercícios do laboratório"
-];
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-const lembrete2: Lembrete = [
-    "Treino",
-    "21/05/2026 16:00"
-];
+function menu(): void {
+    console.log("\n===== MENU =====");
+    console.log("1 - Adicionar lembrete");
+    console.log("2 - Listar lembretes");
+    console.log("3 - Editar lembrete");
+    console.log("4 - Remover lembrete");
+    console.log("0 - Sair");
 
-adicionarLembrete(lembrete1);
-adicionarLembrete(lembrete2);
+    rl.question("Escolha uma opção: ", (opcao) => {
+        switch (opcao) {
+            case "1":
+                adicionar();
+                break;
 
-listarLembretes();
+            case "2":
+                listarLembretes();
+                menu();
+                break;
 
-console.log("\nEditando lembrete...\n");
+            case "3":
+                editar();
+                break;
 
-editarLembrete(0, "Estudar TypeScript Avançado");
+            case "4":
+                remover();
+                break;
 
-listarLembretes();
+            case "0":
+                rl.close();
+                break;
 
-console.log("\nRemovendo lembrete...\n");
+            default:
+                console.log("Opção inválida.");
+                menu();
+        }
+    });
+}
 
-removerLembrete(1);
+function adicionar(): void {
+    rl.question("Título: ", (titulo) => {
+        rl.question("Data de inserção: ", (dataInsercao) => {
+            rl.question("Data limite: ", (dataLimite) => {
+                rl.question("Descrição: ", (descricao) => {
 
-listarLembretes();
+                    const lembrete: Lembrete = [
+                        titulo,
+                        dataInsercao,
+                        dataLimite,
+                        descricao
+                    ];
+
+                    adicionarLembrete(lembrete);
+
+                    console.log("Lembrete adicionado.");
+                    menu();
+                });
+            });
+        });
+    });
+}
+
+function editar(): void {
+    listarLembretes();
+
+    rl.question("Número do lembrete: ", (indice) => {
+        rl.question("Novo título: ", (titulo) => {
+
+            editarLembrete(
+                Number(indice) - 1,
+                titulo
+            );
+
+            console.log("Lembrete atualizado.");
+            menu();
+        });
+    });
+}
+
+function remover(): void {
+    listarLembretes();
+
+    rl.question("Número do lembrete: ", (indice) => {
+
+        removerLembrete(
+            Number(indice) - 1
+        );
+
+        console.log("Lembrete removido.");
+        menu();
+    });
+}
+
+menu();
